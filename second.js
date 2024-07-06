545abb8f7c4538d0f411144ddd59c14ba99b43c8
Kashish Gautam <133134241+kashish1812@users.noreply.github.com>
2024-07-06 15:14:09 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,586 @@
+
+
+function getNum(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        
+        alert("i love you");
+        resolve();
+        },1000);
+
+    });
+}
+ async function demo(){
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+
+ }
+ 
